1517509abbe8265c708bab20d2c4167e7baee5ef
muhammed rifan <rifanmuhammed173@gmail.com>
2023-12-26 22:45:49 +0530

    form validation and form group created

--- src/app/views/teachers-portal/teachers-details-form/teachers-details-form.component.ts
+++ src/app/views/teachers-portal/teachers-details-form/teachers-details-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { DbService } from 'src/app/shared/services/db.service';
 
 @Component({
   selector: 'app-teachers-details-form',
@@ -9,22 +10,37 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class TeachersDetailsFormComponent {
 
 
-  teacherForm!: FormGroup;
+  teacherForm! :FormGroup
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,private service:DbService) {}
 
   ngOnInit() {
-    this.teacherForm = this.fb.group({
-      teacherName: ['', Validators.required],
-      subjectTaught: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phoneNumber: [''],
-      additionalComments: [''],
-    });
+    this.teacherForm = new FormGroup({
+   firstname:new FormControl("",[Validators.required]),
+   lastname:new FormControl("",[Validators.required]),
+   sex:new FormControl("",[Validators.required]),
+   address:new FormControl("",[Validators.required]),
+   dateofbirth:new FormControl("",[Validators.required]),
+   country:new FormControl("",[Validators.required]),
+   state:new FormControl("",[Validators.required]),
+   district:new FormControl("",[Validators.required]),
+   place:new FormControl("",[Validators.required]),
+   h_qualification:new FormControl("",[Validators.required]),
+   degree:new FormControl("",[Validators.required]),
+   graduatedyear:new FormControl("",[Validators.required]),
+   universityname:new FormControl("",[Validators.required]),
+   experience:new FormControl("",[Validators.required]),
+   mobile:new FormControl("",[Validators.required]),
+   position:new FormControl("",[Validators.required]),
+   s_subject:new FormControl("",[Validators.required]),
+   m_status:new FormControl("",[Validators.required]),
+   p_name:new FormControl("",[Validators.required]),
+   p_occupation:new FormControl("",[Validators.required]),
+    })
   }
 
   submitForm() {
-    if (this.teacherForm.valid) {
+    if (this.teacherForm) {
       // Add your form submission logic here
       console.log('Form submitted:', this.teacherForm.value);
 
